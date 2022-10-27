f4bf0ddcee29c8e7c9fe4e0ed082f9012e48c9fe
gmneamul1 <gmneamul1@gmail.com>
2022-10-27 09:50:31 +0600

    home pahe Addedd

--- src/MyHome/MyHome.js
+++ src/MyHome/MyHome.js
@@ -20,8 +20,8 @@ const MyHome = () => {
 
   return (
     <div className=" d-flex justify-content-around">
-      <div className="w-50 mt-5 pt-5">
-        <h1 className="text-center mt-5 pt-5">Achieve Your Skill from <span className="text-warning">Your Home</span></h1>
+      <div className="w-50 mt-5 ">
+        <h1 className="text-center ">Achieve Your Skill from <span className="text-warning">Your Home</span></h1>
         <p className="mt-5">
         The biggest advantage of an online course is that your classroom and instructor (theoretically) are available 24 hours a day, seven days a week. Your only excuse for missing class is not getting online! Otherwise, everything is available to you. You can get announcements, access notes, review assignments, take practice quizzes, discuss questions, chat with fellow students and study any time you want. Other than certain due dates, you make your own schedule for completing the requirements of the course.
 
