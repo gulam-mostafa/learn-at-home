f7c86d2b79961830424c96d8d5413940c8af5b63
gmneamul1 <gmneamul1@gmail.com>
2022-10-27 13:05:56 +0600

    firebase Hosting

--- src/Others/Terms.js
+++ src/Others/Terms.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Terms = () => {
-    return (
-        <div className='vh-100'>
-            <h1>our terms and conditons is don't crime </h1>
-            <p>Go to Register" <Link to="/register"> Register</Link></p>
-        
-        </div>
-    );
+  return (
+    <div className="vh-100">
+      <h1>our terms and conditons is don't crime </h1>
+      <p>
+        Go to Register" <Link to="/register"> Register</Link>
+      </p>
+    </div>
+  );
 };
 
 export default Terms;
